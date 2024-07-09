d915af02e5cad627f890eec3eecdcb0bbe84425c
Ankireddypalleashwani <101695351+Ankireddypalleashwani@users.noreply.github.com>
2024-07-09 05:31:16 +0530

    thi is my loops commit

---
+++ 06-loops.sh
@@ -0,0 +1,13 @@
+#then we can executes the commands again and again then that is a loop
+#two loops commands
+#1 while loops (if we have given expression then we can go with this
+#2 for loop(if we have input then we can go with this)
+
+
+#while loop
+i=10
+  while [$i -gt 0];do
+     echo hello-$i
+     i=$(($i-1))
+     sleep 2
+  done
